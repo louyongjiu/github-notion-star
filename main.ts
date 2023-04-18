95f44c8f1edf9201ce3e8507a61fefab16268843
louyongjiu <921944921@qq.com>
2023-04-18 18:49:15 +0800

    update fullSync

--- main.ts
+++ main.ts
@@ -5,14 +5,14 @@ import assert from 'assert';
 async function fullSync() {
     await Promise.all([github.fullSync(), notion.fullSyncIfNeeded()]);
 
-    await notion.createPages(github.repoList);
+    // await notion.createPages(github.repoList);
     
-    // for (const repo of github.repoList) {
-    //     if (!notion.hasPage(repo.nameWithOwner)) {
-    //         // console.log(`Start to save page: ${JSON.stringify(repo)}`);
-    //         await notion.insertPage(repo);
-    //     }
-    // }
+    for (const repo of github.repoList) {
+        if (!notion.hasPage(repo.nameWithOwner)) {
+            // console.log(`Start to save page: ${JSON.stringify(repo)}`);
+            await notion.insertPage(repo);
+        }
+    }
 }
 
 async function partialSync() {
